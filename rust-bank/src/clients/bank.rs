3be26eecbb7f24c82c709523870141b7bf75dff5
Felipe Lunkes <felipenldev@gmail.com>
2025-03-07 02:00:06 -0300

    Primeira versão

---
+++ rust-bank/src/clients/bank.rs
@@ -0,0 +1,120 @@
+pub mod bank {
+    use std::sync::Mutex;
+
+    #[derive(Clone)]
+    pub struct BankClient {
+        pub(crate) name: String,
+        pub (crate) id: String,
+        account_value: f64
+    }
+
+    struct Bank {
+        clients: Vec<BankClient>
+    }
+
+    impl Bank {
+
+        fn new() -> Self {
+            Bank { clients: Vec::new() }
+        }
+
+        fn add_client(&mut self, name: String, id: String, account_value: f64) {
+            self.clients.push(BankClient { name, id, account_value });
+        }
+
+        fn client_exists(&self, search_id: String) -> bool {
+            self.clients.iter().any(|client| client.id == search_id)
+        }
+
+        fn get_client(&self, search_id: String) -> Option<&BankClient> {
+            self.clients.iter().find(|client| client.id == search_id)
+        }
+
+        pub fn get_client_for_update(&mut self, search_id: String) -> Option<&mut BankClient> {
+            self.clients.iter_mut().find(|client| client.id == search_id)
+        }
+    }
+
+    lazy_static::lazy_static! {
+        static ref BANK: Mutex<Bank> = Mutex::new(Bank::new());
+    }
+
+    pub fn validate_client(client_id: &str) -> Result<(BankClient), String> {
+
+        let bank = BANK.lock().unwrap();
+
+        let client_found = Bank::client_exists(&bank, client_id.to_string());
+
+        if client_found {
+            let bank_client = Bank::get_client(&bank, client_id.to_string());
+            Ok(bank_client.unwrap().clone())
+        } else {
+            Err(format!("Cliente {} não encontrado!", client_id)) }
+    }
+
+    pub fn view_balance(client_id: &str) -> Result<(f64), String> {
+
+        let bank = BANK.lock().unwrap();
+
+        let client_found = Bank::client_exists(&bank, client_id.to_string());
+
+        if !client_found {
+            return Err(format!("Cliente {} não encontrado!", client_id));
+        }
+
+        let client = Bank::get_client(&bank, client_id.to_string()).unwrap();
+
+        Ok(client.account_value)
+    }
+
+    pub fn make_deposit(client_id: &str, value: &f64) -> Result<(), String> {
+
+        let mut bank = BANK.lock().unwrap();
+
+        let client_found = Bank::client_exists(&bank, client_id.to_string());
+
+        if !client_found {
+            return Err(format!("Cliente {} não encontrado!", client_id));
+        }
+
+        let client = Bank::get_client_for_update(&mut bank, client_id.to_string()).unwrap();
+
+        client.account_value += value;
+
+        Ok(())
+    }
+
+    pub fn make_withdrawal(client_id: &str, value: &f64) -> Result<(), String> {
+
+        let mut bank = BANK.lock().unwrap();
+
+        let client_found = Bank::client_exists(&bank, client_id.to_string());
+
+        if !client_found {
+           return Err(format!("Cliente {} não encontrado!", client_id));
+        }
+
+        let client = Bank::get_client_for_update(&mut bank, client_id.to_string()).unwrap();
+
+        if value > &client.account_value {
+            return Err(format!("O saldo do cliente ({}) é menor que o valor pedido para saque {}", client.account_value, &value));
+        }
+
+        client.account_value -= value;
+
+        Ok(())
+    }
+
+    pub fn initialize_db() {
+
+        let mut bank = BANK.lock().unwrap();
+
+        bank.add_client(String::from("Felipe Lunkes"), String::from("001"), 10000.0);
+        bank.add_client(String::from("Gabriel Corrêa"), String::from("002"), 20000.0) ;
+        bank.add_client(String::from("Miguel Lunkes"), String::from("003"), 30000.0);
+
+    }
+
+}
+
+
---
+++ rust-bank/src/main.rs
@@ -0,0 +1,130 @@
+mod clients;
+mod input;
+
+use std::process::exit;
+use clients::{initialize_db, validate_client, make_withdrawal, make_deposit, view_balance};
+use input::{get_user_input};
+
+fn main() {
+
+    initialize_db();
+
+    println!("Seja bem vindo ao rust-bank!");
+
+    let mut client_name: String = String::new();
+    let mut client_id: String = String::new();
+
+    loop {
+        let client_code = get_user_input("Insira seu código de cliente: ");
+
+        if client_code.is_err() {
+            println!("Erro lendo os dados do cliente, tente novamente!");
+        }
+
+        let client_found = validate_client(client_code.unwrap().as_str());
+
+        if client_found.is_err() {
+            println!("Cliente não encontrado, tente novamente!");
+            continue;
+        }
+
+        let client = client_found.unwrap();
+        client_name = client.name.clone();
+        client_id = client.id.clone();
+
+        println!("Seja bem-vindo, {}", client.name);
+
+        while true {
+            println!("Insira a opção para a operação desejada:");
+            println!("1 - Verificar saldo");
+            println!("2 - Fazer aplicação");
+            println!("3 - Fazer saque");
+            println!("4 - Deslogar");
+            println!("5 - Sair");
+
+            let option = get_user_input("Opção: ");
+
+            if option.is_err() {
+                println!("Erro ao obter dados do teclado!");
+                continue;
+            }
+
+            match option.unwrap().as_str() {
+                "1" => app_view_balance(&client_id),
+                "2" => app_make_deposit(&client_id),
+                "3" => app_make_withdrawal(&client_id),
+                "4" => break,
+                "5" => end(&client_name),
+                _ => { println!("Opção inválida, tente novamente!"); }
+            }
+
+            println!("Até a próxima!")
+        }
+    }
+
+    fn end(client_name: &String) {
+        println!("Até a próxima, {}", &client_name);
+        exit(0);
+    }
+
+    fn app_view_balance(client_id: &String) {
+
+        let account_balance = view_balance(&client_id);
+
+        if account_balance.is_err() {
+            println!("Erro: {}", &account_balance.clone().unwrap_err())
+        }
+
+        println!("Seu saldo é de: R$ {}", account_balance.unwrap());
+    }
+
+    fn app_make_deposit(client_id: &String) {
+
+        let value = get_user_input("Insira o valor que deseja depositar (R$): ");
+
+        if value.is_err() {
+            println!("Erro ao obter dados do teclado!");
+            return;
+        }
+
+        let parsed_value: Result<f64, std::num::ParseFloatError> = value.unwrap().parse();
+
+        if parsed_value.is_err() {
+            println!("Número inválido. Tente novamente mais tarde.");
+        }
+
+        let operation_return = make_deposit(&client_id, &parsed_value.as_ref().unwrap());
+
+        if operation_return.is_err() {
+            println!("Erro: {}", operation_return.unwrap_err());
+            return;
+        }
+
+        println!("Sucesso ao realizar o depósito de RS {}!", &parsed_value.unwrap().to_string());
+    }
+
+    fn app_make_withdrawal(client_id: &String) {
+
+        let value = get_user_input("Insira o valor que deseja sacar (R$): ");
+
+        if value.is_err() {
+            println!("Erro ao obter dados do teclado!");
+            return;
+        }
+
+        let parsed_value: Result<f64, std::num::ParseFloatError> = value.unwrap().parse();
+
+        if parsed_value.is_err() {
+            println!("Número inválido. Tente novamente mais tarde.");
+        }
+
+        let operation_return = make_withdrawal(&client_id, &parsed_value.as_ref().unwrap());
+
+        if operation_return.is_err() {
+            println!("Erro: {}", operation_return.unwrap_err());
+            return;
+        }
+
+        println!("Sucesso ao realizar o saque de RS {}!", &parsed_value.unwrap().to_string());
+    }
+}
